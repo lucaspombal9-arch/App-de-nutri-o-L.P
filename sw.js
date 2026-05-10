e10e766c73283dd4cf34a1e096b1e786ac55f821
L.P <lucaspombal9@gmail.com>
2026-05-10 13:17:53 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'lp-nutrition-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './assets/icons/icon-192x192.png',
+  './assets/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(ASSETS);
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE_NAME; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request).then(function(response) {
+        return caches.open(CACHE_NAME).then(function(cache) {
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function() {
+      return caches.match('./index.html');
+    })
+  );
+});
